5ffb5bb11a0732a51a7bae8bd1aaefe1f3f88a13
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-12-11 14:22:20 -0800

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# puppet ssh configuration
+exec { 'ssh_config':
+  path    => '/bin',
+  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
+}
+
