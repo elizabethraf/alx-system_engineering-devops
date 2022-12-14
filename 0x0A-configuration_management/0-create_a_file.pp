21df81d0a93dcf8954dcad8e14e014f7c9e4fad9
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-12-14 01:06:19 -0800

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# creates a puppet file in the /tmp.
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+
