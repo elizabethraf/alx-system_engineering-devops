248bf0886104aa2d1d4740580703605c2b88f3a3
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-01-31 20:16:26 +0000

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,10 +3,13 @@
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Display reddit API"""
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:109.0) Gecko/20100101 Firefox/109.0'}
-    response = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit), headers=headers, allow_redirects=False)
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:109.0) \
+              Gecko/20100101 Firefox/109.0'}
+    response = requests.get('https://www.reddit.com/r/{}/about.json'.format(
+                            subreddit), headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
