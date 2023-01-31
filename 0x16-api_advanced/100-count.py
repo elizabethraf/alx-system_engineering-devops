585c968d17228671213bb096e18d706eace6e153
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-01-31 20:43:55 +0000

    100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,20 +5,18 @@ import requests
 import re
 from collections import defaultdict
 
+
 def count_words(subreddit, word_list, after=None, counts=defaultdict(int)):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36"
-    }
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+               AppleWebKit/537.36 (KHTML, like Gecko)\
+               Chrome/83.0.4103.61 Safari/537.36"}
     params = {
         "limit": 100,
         "t": "all",
         "after": after,
     }
-    response = requests.get(
-        f"https://www.reddit.com/r/{subreddit}/hot/.json",
-        headers=headers,
-        params=params,
-    )
+    response = requests.get("https://www.reddit.com/r/{}/hot/.json".format(
+                            subreddit), headers=headers, params=params)
 
     if response.status_code == 404:
         return
@@ -40,4 +38,3 @@ def count_words(subreddit, word_list, after=None, counts=defaultdict(int)):
         sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
         for word, count in sorted_counts:
             print(word, count)
-
