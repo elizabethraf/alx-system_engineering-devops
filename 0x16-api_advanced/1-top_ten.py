60b4a4ed9f42d3ee5d01553a17f9a1843a76e4fe
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-01-31 20:23:40 +0000

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,11 +10,11 @@ def top_ten(subreddit):
     """Read reddit API"""
 
     headers = {'User-Agent': 'MyBot/1.0'}
-    response = requests.get(f'https://www.reddit.com/r/{subreddit}/hot.json', headers=headers, params={'limit': 10})
+    response = requests.get('https://www.reddit.com/r/{}/hot.json'.format(
+                            subreddit), headers=headers, params={'limit': 10})
     if response.status_code == 200:
         data = response.json()
         for post in data['data']['children']:
             print(post['data']['title'])
     else:
         print(None)
-
