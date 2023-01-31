0b871c519f685f93af42a5da8f244cf1a9b0593b
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-01-31 15:50:13 +0000

    100-count.py

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Display infomation from API Reddit"""
+
+import requests
+import re
+from collections import defaultdict
+
+def count_words(subreddit, word_list, after=None, counts=defaultdict(int)):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36"
+    }
+    params = {
+        "limit": 100,
+        "t": "all",
+        "after": after,
+    }
+    response = requests.get(
+        f"https://www.reddit.com/r/{subreddit}/hot/.json",
+        headers=headers,
+        params=params,
+    )
+
+    if response.status_code == 404:
+        return
+
+    data = response.json()
+    children = data["data"]["children"]
+    after = data["data"]["after"]
+
+    for child in children:
+        title = child["data"]["title"]
+        words = re.findall(r"\b\w+\b", title.lower())
+        for word in words:
+            if word in word_list:
+                counts[word] += 1
+
+    if after:
+        count_words(subreddit, word_list, after, counts)
+    else:
+        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in sorted_counts:
+            print(word, count)
+
