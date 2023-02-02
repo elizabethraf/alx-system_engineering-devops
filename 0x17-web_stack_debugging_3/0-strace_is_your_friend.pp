abee756af7a845a162a9251fd6d60a9902820d7d
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-02-02 17:28:50 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,11 @@
-class apache_fix {
-  file_line { 'modify_wp_settings':
-    path    => '/var/www/html/wp-settings.php',
-    line    => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );',
-    replace => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.phpp\' );',
-  }
+# Fixes bad filename
+exec { 'fix-wordpress':
+  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
+  notify => Service['apache2'],
+}
 
-  service { 'apache2':
-    ensure => running,
-    enable => true,
-    subscribe => File_line['modify_wp_settings'],
-    refreshonly => true,
-  }
+service { 'apache2':
+  ensure => running,
+  restart => 'true',
+  subscribe => Exec['fix-wordpress'],
 }
