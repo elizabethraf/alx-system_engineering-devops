290e5ca7af47f13348b686f6f662ea508bf2e16d
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-02-02 17:32:38 +0000

    0-strace_is_your_friend.pp lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,12 @@
 # Fixes bad filename
 exec { 'fix-wordpress':
   command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
-  notify => Service['apache2'],
+  notify  => Service['apache2'],
 }
 
 service { 'apache2':
-  ensure => running,
-  restart => 'true',
+  ensure    => running,
+  restart   => true,
   subscribe => Exec['fix-wordpress'],
 }
+
